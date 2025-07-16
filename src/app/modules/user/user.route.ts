ef8b418a2b07649287aa431345643313e69b11ef
safin33221 <safin33221@gmail.com>
2025-07-16 16:53:24 +0600

    implement middle ware for role based accessed

--- src/app/modules/user/user.route.ts
+++ src/app/modules/user/user.route.ts
@@ -4,11 +4,35 @@ import { UserControllers } from "./user.controllers";
 import z, { AnyZodObject } from "zod";
 import { createUserSchema } from "./user.validation";
 import { validateRequest } from "../../middlewares/validateRequest";
-
+import jwt, { JwtPayload } from 'jsonwebtoken';
+import AppError from "../../../errorHelpers/AppError";
+import { Role } from "./user.interface";
 
 
 const router = Router()
 router.post("/register", validateRequest(createUserSchema), UserControllers.createUser)
-router.get("/all-users", UserControllers.getAllUsers)
+router.get("/all-users", async (req: Request, res: Response, next: NextFunction) => {
+
+    try {
+
+        const accessToken = req.headers.authorization
+        if (!accessToken) {
+            throw new AppError(403, "No Token Received")
+        }
+        const verifyToken = jwt.verify(accessToken, "secret")
+
+        if (!verifyToken) {
+
+            throw new AppError(403, "You'r are not authorized")
+        }
+        if ((verifyToken as JwtPayload).role !== Role.ADMIN ) {
+            throw new AppError(403, "You'r not permitted to view this route")
+        }
+
+        next()
+    } catch (error) {
+        next(error)
+    }
+}, UserControllers.getAllUsers)
 
 export const userRoutes = router  
