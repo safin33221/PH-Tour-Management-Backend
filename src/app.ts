8fa916ad20b64737eb9d18e73efdf15cff8f7623
safin33221 <safin33221@gmail.com>
2025-07-22 00:53:27 +0600

    completed part#

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,23 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars, @typescript-eslint/no-explicit-any
 import express, { Request, Response } from 'express'
 import cors from 'cors'
 import { router } from './app/routes'
+import "./config/passpost"
 import { globalErrorhandler } from './app/middlewares/globalErrorHandler'
 import notFound from './app/middlewares/notFound'
 import cookieParser from 'cookie-parser'
+import passport from 'passport';
+import expressSession from 'express-session';
 const app = express()
 
+
+app.use(expressSession({
+    secret: "secret",
+    resave: false,
+    saveUninitialized: false
+}))
+app.use(passport.initialize())
+app.use(passport.session())
 app.use(cookieParser())
 app.use(express.json())
 app.use(cors())
@@ -18,7 +30,6 @@ app.get('/', (req: Request, res: Response) => {
     )
 })
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars, @typescript-eslint/no-explicit-any
 app.use(globalErrorhandler)
 
 app.use(notFound)
--- src/app/modules/auth/auth.controller.ts
+++ src/app/modules/auth/auth.controller.ts
@@ -7,6 +7,10 @@ import { User } from "../user/user.model"
 import { authServices } from "./auth.service"
 import AppError from "../../../errorHelpers/AppError"
 import { setAuthCookie } from "../../../utils/setCookie"
+import { JwtPayload } from "jsonwebtoken"
+import { generateToken } from "../../../utils/jwt"
+import { createToken } from "../../../utils/userTokens"
+import { envVars } from "../../../config/env"
 
 const credentialLogin = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
     const loginInfo = await authServices.credentialLogin(req.body)
@@ -68,7 +72,7 @@ const logout = catchAsync(async (req: Request, res: Response, next: NextFunction
 
 const resetPassword = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
 
-    const decodedToken = req.user;
+    const decodedToken = req.user as JwtPayload
     const newPassword = req.body.newPassword
     const oldPassword = req.body.oldPassword
     await authServices.resetPassword(oldPassword, newPassword, decodedToken)
@@ -82,8 +86,30 @@ const resetPassword = catchAsync(async (req: Request, res: Response, next: NextF
     })
 })
 
+
+const googleCallback = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
+
+    let redirectTo = req.query.state ? req.query.state as string : ""
+    console.log("redirect to", redirectTo);
+    if (redirectTo.startsWith("/")) {
+        redirectTo = redirectTo.slice(1)
+
+    }
+    const user = req.user
+    // console.log("User>>>>>>>>>", user);
+    if (!user) {
+        throw new AppError(httpStatus.NOT_FOUND, "User not found ")
+    }
+    const tokenInfo = await createToken(user)
+    setAuthCookie(res, tokenInfo)
+
+    res.redirect(`${envVars.FRONTEND_URL}/${redirectTo}`)
+})
+
+
+
 export const authControllers = {
     credentialLogin,
     getNewAccessToken,
-    logout, resetPassword
+    logout, resetPassword, googleCallback
 }
