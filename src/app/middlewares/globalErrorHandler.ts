1f2d56574ca6f4d9cdc6b7a8b4f17f12d74d31e3
safin33221 <safin33221@gmail.com>
2025-07-23 01:46:58 +0600

    Handle duplicate error on mongoose

--- src/app/middlewares/globalErrorHandler.ts
+++ src/app/middlewares/globalErrorHandler.ts
@@ -6,9 +6,18 @@ import AppError from "../../errorHelpers/AppError"
 
 
 export const globalErrorhandler = (err: any, req: Request, res: Response, next: NextFunction) => {
+    console.log(err.message);
     let statusCode = 500
     let message = `something went wrong !!`
-    if (err instanceof AppError) {
+
+    if (err.code === 11000) {
+        const matchArray = err.message.match(/"([^"]*)"/)
+
+        statusCode = 400;
+        message = `${matchArray[1]} already exist`
+
+    }
+    else if (err instanceof AppError) {
         statusCode = err.statusCode
         message = err.message
     } else if (err instanceof Error) {
