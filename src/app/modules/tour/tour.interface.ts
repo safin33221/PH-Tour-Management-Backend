a3b30ef4f158545ce3704b324b1e16658f8e2432
safin33221 <safin33221@gmail.com>
2025-07-25 00:32:26 +0600

    Create Tour modal

--- src/app/modules/tour/tour.interface.ts
+++ src/app/modules/tour/tour.interface.ts
@@ -1,18 +1,21 @@
 import { Types } from "mongoose";
 
+export interface ITourType{
+    name:string
+}
 export interface ITour {
     title: string;
     slug: string;
     description?: string;
     Images?: string[];
-    location?: string[];
+    location?: string;
     costFrom?: string;
     startDate?: Date;
     endDate?: Date;
     included?: string[];
     excluded?: string[];
     amenities?: string[];
-    tourPlan?: string;
+    tourPlan?: string[];
     maxGuest?: number;
     minAge?: number;
     division: Types.ObjectId;
