da4cdb03a7de19a4019a6ce021e4e5bce61f40f6
safin33221 <safin33221@gmail.com>
2025-07-27 12:31:21 +0600

    Configure Evn file with sll paymet info

--- src/app/config/env.ts
+++ src/app/config/env.ts
@@ -19,6 +19,12 @@ interface EnvConfig {
     GOOGLE_CALLBACK_URL: string,
     EXPRESS_SESSION_SECRET: string,
     FRONTEND_URL: string,
+    SSL: {
+        SSL_STORE_ID: string,
+        SSL_STORE_PASS: string,
+        SSL_PAYMENT_API: string,
+        SSL_VALIDATION_API: string,
+    }
 }
 
 const loadEnvVariables = (): EnvConfig => {
@@ -33,6 +39,10 @@ const loadEnvVariables = (): EnvConfig => {
         "GOOGLE_CALLBACK_URL",
         "EXPRESS_SESSION_SECRET",
         "FRONTEND_URL",
+        "SSL_STORE_ID",
+        "SSL_STORE_PASS",
+        "SSL_PAYMENT_API",
+        "SSL_VALIDATION_API"
     ];
 
     requiredEnvVariables.forEach(key => {
@@ -58,6 +68,12 @@ const loadEnvVariables = (): EnvConfig => {
         GOOGLE_CALLBACK_URL: process.env.GOOGLE_CALLBACK_URL as string,
         EXPRESS_SESSION_SECRET: process.env.EXPRESS_SESSION_SECRET as string,
         FRONTEND_URL: process.env.FRONTEND_URL as string,
+        SSL: {
+            SSL_STORE_ID: process.env.SSL_STORE_ID as string,
+            SSL_STORE_PASS: process.env.SSL_STORE_PASS as string,
+            SSL_PAYMENT_API: process.env.SSL_PAYMENT_API as string,
+            SSL_VALIDATION_API: process.env.SSL_VALIDATION_API as string,
+        }
 
     }
 }
