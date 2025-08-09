e5a66ac6c742e5d51d9a1a30e663d2f3bc2a0835
safin33221 <safin33221@gmail.com>
2025-08-10 02:03:04 +0600

    config redis

--- src/app/config/env.ts
+++ src/app/config/env.ts
@@ -42,6 +42,12 @@ interface EnvConfig {
         SMTP_FROM: string,
         SMTP_HOST: string,
         SMTP_PORT: string,
+    },
+    REDIS: {
+        REDIS_HOST: string,
+        REDIS_PORT: string,
+        REDIS_USERNAME: string,
+        REDIS_PASSWORD: string,
     }
 }
 
@@ -75,6 +81,10 @@ const loadEnvVariables = (): EnvConfig => {
         "SMTP_FROM",
         "SMTP_HOST",
         "SMTP_PORT",
+        "REDIS_HOST",
+        "REDIS_PORT",
+        "REDIS_USERNAME",
+        "REDIS_PASSWORD",
     ];
 
     requiredEnvVariables.forEach(key => {
@@ -125,6 +135,12 @@ const loadEnvVariables = (): EnvConfig => {
             SMTP_FROM: process.env.SMTP_FROM as string,
             SMTP_HOST: process.env.SMTP_HOST as string,
             SMTP_PORT: process.env.SMTP_PORT as string,
+        },
+        REDIS: {
+            REDIS_HOST: process.env.REDIS_HOST as string,
+            REDIS_PORT: process.env.REDIS_PORT as string,
+            REDIS_USERNAME: process.env.REDIS_USERNAME as string,
+            REDIS_PASSWORD: process.env.REDIS_PASSWORD as string,
         }
 
     }
--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,7 @@ import mongoose from 'mongoose';
 import app from './app';
 import { envVars } from './app/config/env';
 import { seedSuperAdmin } from './app/utils/seedSuperAdmin';
+import { connectRedis } from './app/config/redis.config';
 
 
 
@@ -23,6 +24,7 @@ const startSever = async () => {
     }
 }
 (async () => {
+    await connectRedis()
     await startSever()
     await seedSuperAdmin()
 })()
